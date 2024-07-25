be7d8f1e5486fc370e7bd1cb1e4c2338cc188cd0
jitixa <jitixa.kothiya@envertis.com.au>
2024-07-25 10:51:55 +1000

    add new changes 25-07-24

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"; // Importing useState hook from React
+import logo from "./logo.svg"; // Importing logo (assuming it's used elsewhere in the application)
+import "./App.css"; // Importing CSS file for styling
 
 function App() {
+  // Defining the state variable 'currentTime' and the function 'setTime' to update it
+  const [currentTime, setTime] = useState(
+    "Hello Jitixa! When you Click On This Button Get Current Time"
+  );
+
+  // Function to update the 'currentTime' state with the current time
+  function updateTime() {
+    let getTime = new Date().toLocaleTimeString(); // Getting the current time as a formatted string
+    setTime(getTime); // Updating the state with the current time
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>{currentTime}</h2> {/* Displaying the current time or message */}
+        <button className="btn" onClick={updateTime}>
+          {" "}
+          {/* Button to update the time when clicked */}
+          Get Time
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; // Exporting the App component as the default export
